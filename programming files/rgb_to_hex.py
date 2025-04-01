eb1de68bf9cfa82edc4a397058c6e9fbedd10523
Kristof Rohaly-Medved <128512492+kristofrm@users.noreply.github.com>
2025-04-01 10:27:10 -0400

    Update rgb_to_hex.py

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,7 +1,9 @@
 def rgb_to_hex(r, g, b):
+    # Gets maximum value in range of 0 to minimum of 255 to r, g, b value
     r = max(0, min(255, r))
     g = max(0, min(255, g))
     b = max(0, min(255, b))
+    # Returns r, g, b values in hex format
     return '{:02X}{:02X}{:02X}'.format(r, g, b)
 
 
